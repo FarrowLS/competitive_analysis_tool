1809c53cc3098804aca05bfa25bdbcc59ff9d7ed
FarrowLS <peterwfarrow@yahoo.com>
2013-12-01 14:46:04 -0500

    First commit of competitive_analysis_tool, clean install of Rails 4.0.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CompetitiveAnalysisTool::Application.config.secret_key_base = '29ef60df90f7441e272cff6e2212c1828c40f88b25716ba6300e61a28433091ecc6d8ff566c9b341e87ee0a898b277754ee8fdb31ab26992c1220e405f03e7d2'
