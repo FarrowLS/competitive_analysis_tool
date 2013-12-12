6185370c5b6be57c9b77abb7b5db5e474b3aac5d
FarrowLS <peterwfarrow@yahoo.com>
2013-12-12 00:55:22 -0500

    Updated seed placeholder info

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,14 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Company.create(name: "Name 1", pricing_information: "Pricing Information 1<p><ul><li>item 1</li><li>item 2</li></ul></p>", subject_matter_comparison: "Subject Matter Comparison 1", length: "Length 1", configuration: "Configuration 1", certification: "Certification 1", noted_customers: "Noted Customers 1")
+Company.create(name: "Name 1", quick_pitch: "Quick Pitch 1", strengths: "Strengths 1", weaknesses: "Weaknesses 1", pricing_information: "Pricing Information 1<p><ul><li>item 1</li><li>item 2</li></ul></p>", subject_matter_comparison: "Subject Matter Comparison 1", length: "Length 1", configuration: "Configuration 1", certification: "Certification 1", noted_customers: "Noted Customers 1", links: "Links 1")
 
-Company.create(name: "Name 2", pricing_information: "Pricing Information 2", subject_matter_comparison: "Subject Matter Comparison 2", length: "Length 2", configuration: "Configuration 2", certification: "Certification 2", noted_customers: "Noted Customers 2")
+Company.create(name: "Name 2", quick_pitch: "Quick Pitch 2", strengths: "Strengths 2", weaknesses: "Weaknesses 2", pricing_information: "Pricing Information 2<p><ul><li>item 1</li><li>item 2</li></ul></p>", subject_matter_comparison: "Subject Matter Comparison 2", length: "Length 2", configuration: "Configuration 2", certification: "Certification 2", noted_customers: "Noted Customers 2", links: "Links 2")
 
-Company.create(name: "Name 3", pricing_information: "Pricing Information 3", subject_matter_comparison: "Subject Matter Comparison 3", length: "Length 3", configuration: "Configuration 3", certification: "Certification 3", noted_customers: "Noted Customers 3")
+Company.create(name: "Name 3", quick_pitch: "Quick Pitch 3", strengths: "Strengths 3", weaknesses: "Weaknesses 3", pricing_information: "Pricing Information 3<p><ul><li>item 1</li><li>item 2</li></ul></p>", subject_matter_comparison: "Subject Matter Comparison 3", length: "Length 3", configuration: "Configuration 3", certification: "Certification 3", noted_customers: "Noted Customers 3", links: "Links 3")
+
+Company.create(name: "Name 4", quick_pitch: "Quick Pitch 4", strengths: "Strengths 4", weaknesses: "Weaknesses 4", pricing_information: "Pricing Information 4<p><ul><li>item 1</li><li>item 2</li></ul></p>", subject_matter_comparison: "Subject Matter Comparison 4", length: "Length 4", configuration: "Configuration 4", certification: "Certification 4", noted_customers: "Noted Customers 4", links: "Links 4")
+
+Company.create(name: "Name 5", quick_pitch: "Quick Pitch 5", strengths: "Strengths 5", weaknesses: "Weaknesses 5", pricing_information: "Pricing Information 5<p><ul><li>item 1</li><li>item 2</li></ul></p>", subject_matter_comparison: "Subject Matter Comparison 5", length: "Length 5", configuration: "Configuration 5", certification: "Certification 5", noted_customers: "Noted Customers 5", links: "Links 5")
 
 
