5335b16b7940bd02b18a1c8754ce7bb69bc19078
FarrowLS <peterwfarrow@yahoo.com>
2013-12-11 22:44:06 -0500

    Allowed HTML to be viewed in views

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Company.create(name: "Name 1", pricing_information: "Pricing Information 1", subject_matter_comparison: "Subject Matter Comparison 1", length: "Length 1", configuration: "Configuration 1", certification: "Certification 1", noted_customers: "Noted Customers 1")
+Company.create(name: "Name 1", pricing_information: "Pricing Information 1<p><ul><li>item 1</li><li>item 2</li></ul></p>", subject_matter_comparison: "Subject Matter Comparison 1", length: "Length 1", configuration: "Configuration 1", certification: "Certification 1", noted_customers: "Noted Customers 1")
+
+Company.create(name: "Name 2", pricing_information: "Pricing Information 2", subject_matter_comparison: "Subject Matter Comparison 2", length: "Length 2", configuration: "Configuration 2", certification: "Certification 2", noted_customers: "Noted Customers 2")
+
+Company.create(name: "Name 3", pricing_information: "Pricing Information 3", subject_matter_comparison: "Subject Matter Comparison 3", length: "Length 3", configuration: "Configuration 3", certification: "Certification 3", noted_customers: "Noted Customers 3")
 
 
